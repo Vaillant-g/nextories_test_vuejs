2c1bb3a4f1a9dc6716ecc6bd979740fb5b144b5c
gautier <gautier.vaillant@epitech.eu>
2020-09-28 01:04:28 +0200

    setup projet vue & exercice 1

---
+++ inventaire/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Calculatrice msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import Calculatrice from "./components/Calculatrice.vue";
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+export default {
+  name: "App",
+  components: {
+    Calculatrice
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ inventaire/src/components/Calculatrice.vue
@@ -0,0 +1,99 @@
+<template>
+  <b-container>
+    <div class="calculatrice">
+      <h1>Calculatrice</h1>
+      <b-row>
+        <b-col class="entries">
+          <b-row class="nom_meuble my-2">
+            <b-col>
+              <b-form-input
+                id="input-default"
+                placeholder="Nom du meuble"
+                v-model="nom_meuble"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+          <!-- nom -->
+          <b-row class="longueur_meuble my-2">
+            <b-col>
+              <b-form-input
+                id="input-default"
+                placeholder="Longueur du meuble (Cm)"
+                v-model="longueur_meuble"
+                type="number"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+          <!-- longueur -->
+          <b-row class="largeur_meuble my-2">
+            <b-col>
+              <b-form-input
+                id="input-default"
+                placeholder="Largeur du meuble (Cm)"
+                v-model="largeur_meuble"
+                type="number"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+          <!-- largeur -->
+          <b-row class="hauteur_meuble my-2">
+            <!-- <b-col md="2">
+            <label for="hauteur_meuble">Hauteur du meuble:</label>
+          </b-col> -->
+            <b-col>
+              <b-form-input
+                id="input-default"
+                placeholder="Hauteur du meuble (Cm)"
+                v-model="hauteur_meuble"
+                type="number"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+          <!-- hauteur -->
+        </b-col>
+
+        <b-col class="volume">
+          <b-jumbotron class="my-1">
+            <p>Le volume du meuble est de {{ volume_meuble }} m3</p>
+          </b-jumbotron>
+        </b-col>
+      </b-row>
+    </div>
+  </b-container>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+export default {
+  name: "CalculatriceItem",
+  props: {},
+  data: function() {
+    return {
+      nom_meuble: "",
+      longueur_meuble: null,
+      largeur_meuble: null,
+      hauteur_meuble: null,
+      aire: null,
+      message: ""
+    };
+  },
+  computed: {
+    volume_meuble: function() {
+      return (
+        (this.longueur_meuble / 100 ) * (this.largeur_meuble  / 100 ) * (this.hauteur_meuble / 100 )
+      );
+    }
+  },
+  methods: {
+    onlyNumber: function() {}
+  }
+};
+</script>
+
+<style scoped>
+.calculatrice label {
+  text-align: right;
+}
+</style>
